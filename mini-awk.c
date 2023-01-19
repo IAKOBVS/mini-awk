687bc3cd11983c180a5fcfc232150f9beda0e6ea
James Halim <tirtajames45@gmail.com>
2023-01-19 16:22:32 +0700

    newest

--- mini-awk.c
+++ mini-awk.c
@@ -3,26 +3,26 @@
 
 #define MAX_LENGTH 512
 
-void whichStr(int nStr, char delim)
+void whichStrSpc(int nStr, char delim)
 {
 	char inStr[MAX_LENGTH];
+	/* copies stdin to var */
 	fgets(inStr, MAX_LENGTH, stdin);
-
 	int i = 0;
 	while (inStr[i] == delim && inStr[i]) ++i;
-
 	if (nStr)
 		for (int nDelim = 1; nDelim<nStr && inStr[i]; ++nDelim) {
-			while (inStr[i] != delim && inStr[i]) ++i;
-			while (inStr[i] == delim && inStr[i]) ++i;
+			while (inStr[i] != delim && inStr[i])
+				++i;
+			while (inStr[i] == delim && inStr[i])
+				++i;
 		}
-
-	for (; inStr[i] != delim && inStr[i] != '\0' ; ++i)
+	for (; inStr[i] != delim && inStr[i]; ++i)
 		printf("%c", inStr[i]);
 }
 
 int main(int argc, char *argv[])
 {
-	whichStr(strtol(argv[1], NULL, 10), ' ');
+	whichStrSpc(strtol(argv[1], NULL, 10), ' ');
 	return 0;
 }
